ad384553b57fd69c61475fdb78fc9fc3a7f288c8
Josh Engler <55295030+jdengler@users.noreply.github.com>
2021-01-22 16:03:12 -0600

    Add files via upload

---
+++ Lab2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        public const double CON_FAC = 0.413;
+        public const int FEET_IN_MILE = 5280;
+
+        static void Main(string[] args)
+        {
+            //Starter code for lab 2
+            Console.WriteLine("Please enter your height in inches: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the number of steps taken in a day: ");
+            int steps = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("You walked " + calcDistance(height, steps) + " miles");
+            Console.ReadLine();
+        }
+
+        public static double calcDistance(int height, int steps)
+        {
+            double strideLength = height * CON_FAC;
+            strideLength /= 12;
+
+            double feetWalked = strideLength * steps;
+            double milesWalked = feetWalked / FEET_IN_MILE;
+
+            milesWalked = Math.Round(milesWalked, 2);
+
+            return milesWalked;
+
+        }
+    }
+}
